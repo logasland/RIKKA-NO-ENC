7f786f7c1c2d006cde1247f136361a53ea36428b
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-12 18:28:59 +0700

    Update donasi.js

--- commands/special/donasi.js
+++ commands/special/donasi.js
@@ -6,7 +6,7 @@ exports.run = {
       client.sendMessageModify(m.chat, info(), m, {
             title: 'D O N A S I',
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/035848cbd019551eac62f.jpg')
+            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/51269b4727fbf6ff01f3c.jpg')
             })         
    },
    error: false,
@@ -23,9 +23,9 @@ let info = () => {
 ┃Bagi Anda yang memiliki saldo lebih
 ┃bisa di donasikan melalui berikut ini.
 ┃
-┃D A N A : [ 0896-5389-6400 ]
-┃P U L S A : [ 0851-5697-2228 ]
-┃G O P A Y : [ 0896-5389-6400 ]
+┃D A N A : [ 0857-3079-4089 ]
+┃P U L S A : [ 0857-3079-4089 ]
+┃G O P A Y : [ 0857-3079-4089 ]
 ┗⬣
 
 note : *donasi seikhlasnya aja*`
